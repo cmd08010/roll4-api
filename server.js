ec31437717b82492cf139a7a1739c2c4346392c8
cmd08010 <colleendunion@gmail.com>
2021-03-04 18:17:18 -0500

    update server for deploy

--- server.js
+++ server.js
@@ -68,9 +68,6 @@ app.get('/', (req, res) => {
   res.send('Deployed')
 })
 
-app.get('/*', (req, res, next) => {
-  res.send('Deployed')
-})
 // register error handling middleware
 // note that this comes after the route middlewares, because it needs to be
 // passed any error messages from them
